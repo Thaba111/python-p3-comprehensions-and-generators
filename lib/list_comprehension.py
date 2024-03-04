25ed197e17e53abe099cc1404ebcdbceda06a939
Edith Thaba <writersherry5@gmail.com>
2024-03-04 07:54:46 +0300

    Done with Assignemnt

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers= [n for n in num_list if n%2 == 0]
+    return even_numbers
+num_list=([0, 1, 3, 5, 7, 8, 9])
+print(return_evens(num_list))
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [sentence + "!" for sentence in sentence_list]
+    return exclamation_list
+sentence_list = [
+            "I like computers",
+            "I require coffee",
+            "Live long and prosper",
+        ]
+print(make_exclamation(sentence_list))
